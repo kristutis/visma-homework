26f2fcdaf4441adc16a461d6d0265e6bec976a83
kristutis <kristijonas.karalaitis@gmail.com>
2021-04-16 18:24:50 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            while (x != "quit")
+            {
+                x = Console.ReadLine();
+                Console.WriteLine(x);
+            }            
+        }
+    }
+    
+    public class Book
+    {
+
+    }
+}
